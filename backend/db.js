846279561d421b8dd95317b948a200fa021530a5
Jon Paul Uritis <jonpaul.uritis@thestormcloudgroup.com>
2018-11-29 19:14:09 -0800

    db connection

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
-const conn = require('env');
+const conn = require('./env');
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(conn);
 const User = require('./models/User')(sequelize, Sequelize);
