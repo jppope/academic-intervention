c072fdf3aa5f7cb4e09e33befaa4297fc18bc48a
briantmorr <briantmorr@gmail.com>
2019-01-24 20:11:47 -0800

    get requests to /paper now show results from the database

---
+++ backend/models/Paper.js
@@ -0,0 +1,57 @@
+module.exports = function (sequelize, DataTypes) {
+	return sequelize.define('paper', {
+		id: {
+			type: DataTypes.INTEGER(10).UNSIGNED,
+			allowNull: false,
+			primaryKey: true,
+			autoIncrement: true
+        },
+		title: {
+			type: DataTypes.STRING(255),
+			allowNull: false
+        },
+        desc: {
+			type: DataTypes.TEXT('medium'),
+			allowNull: true
+		},
+		author: {
+			type: DataTypes.STRING(255),
+			allowNull: false,
+			unique: true
+        },
+        source_url: {
+            type: DataTypes.STRING(255),
+            allowNull: false
+        },
+		site: {
+			type: DataTypes.STRING(255),
+			allowNull: true
+		},
+		group_id: {
+			type: DataTypes.INTEGER(11),
+			allowNull: true
+		},
+		verified: {
+			type: DataTypes.INTEGER(1),
+			allowNull: false
+		},
+		date_published: {
+			type: DataTypes.DATE,
+			allowNull: true
+		},
+		deleted_at: {
+			type: DataTypes.DATE,
+			allowNull: true
+		},
+		created_at: {
+			type: DataTypes.DATE,
+			allowNull: true
+		},
+		updated_at: {
+			type: DataTypes.DATE,
+			allowNull: true
+		},	
+	}, {
+		tableName: 'paper'
+	});
+};
